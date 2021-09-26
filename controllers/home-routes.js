6d6e0bd2ab33c1eee950544f25f23cde21dabd14
ronarceo <ronaldmarceo@gmail.com>
2021-09-25 22:34:10 -0500

    improved styling of login view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,13 +43,13 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirects the request to another route
     if (req.session.logged_in) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
-  
+
     res.render('login');
-  });
+});
   
-  module.exports = router;
+
 
 module.exports = router;
