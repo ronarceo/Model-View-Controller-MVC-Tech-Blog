fc8941b83c4af0b033a3d0b9c38f5451c7346e22
ronarceo <ronaldmarceo@gmail.com>
2021-09-27 16:26:43 -0500

    moved delete to edit post handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,26 +21,6 @@ const newPostHandler = async (event) => {
   }
 };
 
-const deletePostHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete post');
-    }
-  }
-};
-
 document
   .querySelector('.new-post-form')
   .addEventListener('submit', newPostHandler);
-
-document
-  .querySelector('#deletePostButton')
-  .addEventListener('click', deletePostHandler);
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -26,6 +26,26 @@ const editPostHandler = async (event) => {
     }
 };
 
+const deletePostHandler = async (event) => {
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to delete post');
+    }
+};
+
 document
     .querySelector('#updatePostButton')
     .addEventListener('click', editPostHandler);
+
+document
+    .querySelector('#deletePostButton')
+    .addEventListener('click', deletePostHandler);
