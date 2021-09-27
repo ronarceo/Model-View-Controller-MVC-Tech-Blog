a773d117500434eedaba7d039ee9b9c3875a8928
ronarceo <ronaldmarceo@gmail.com>
2021-09-26 23:51:58 -0500

    made login and signup separate views and scripts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,13 +41,23 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirects the request to another route
+    // If the user is already logged in, redirects the request to homepage
     if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
 
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirects the request to homepage
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
 });
 
 router.get('/post/:id', async (req, res) => {
