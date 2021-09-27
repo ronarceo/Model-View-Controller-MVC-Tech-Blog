1c4ee93c3b2d622b8c80ce622ff66a1f904207a4
ronarceo <ronaldmarceo@gmail.com>
2021-09-26 22:37:55 -0500

    users are now able to update posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,4 +42,49 @@ router.get('/', withAuth, async (req, res) => {
     }
 });
 
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        postData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'post_text',
+                'title',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+
+        res.render('edit-post', {
+            post,
+            logged_in: true
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
---
+++ public/js/edit-post.js
@@ -0,0 +1,31 @@
+const editPostHandler = async (event) => {
+    event.preventDefault();
+    console.log('editPostHandler fired');
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const title = document.querySelector('#post-title').value.trim();
+    const post_text = document.querySelector('#post-text').value.trim();
+
+    if (title && post_text) {
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, post_text }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to edit post');
+        }
+    }
+};
+
+document
+    .querySelector('#updatePostButton')
+    .addEventListener('click', editPostHandler);
